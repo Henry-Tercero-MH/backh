3215974300826c7c3e3eba1bb24659ee5657444e
Henry-Tercero-MH <tercerohernandezhenrymisael@gmail.com>
2025-02-07 20:20:49 -0600

    se corrijio el envrioment

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 require("dotenv").config();
 
 // Validación de las variables de entorno
@@ -17,17 +17,22 @@ if (
 // Creación del pool de conexiones
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 46724,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "sistema_ventas",
+  database: process.env.DB_NAME || "tienda_online",
 });
 
 // Manejo del cierre del pool
-process.on("SIGINT", async () => {
-  await pool.end();
-  console.log("Conexión a la base de datos cerrada.");
-  process.exit(0);
+process.on("SIGINT", () => {
+  pool.end((err) => {
+    if (err) {
+      console.error("Error al cerrar la conexión a la base de datos:", err);
+    } else {
+      console.log("Conexión a la base de datos cerrada.");
+    }
+    process.exit(0);
+  });
 });
 
 module.exports = pool;
