8c684d6331d6eabb2cb9caa9f582b75b1a614d3c
Henry-Tercero-MH <tercerohernandezhenrymisael@gmail.com>
2025-02-07 18:26:04 -0600

    se cargo a realway

--- config/db.js
+++ config/db.js
@@ -1,18 +1,33 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+// Validación de las variables de entorno
+if (
+  !process.env.DB_HOST ||
+  !process.env.DB_PORT ||
+  !process.env.DB_USER ||
+  !process.env.DB_PASSWORD ||
+  !process.env.DB_NAME
+) {
+  throw new Error(
+    "Faltan variables de entorno para la configuración de la base de datos."
+  );
+}
+
+// Creación del pool de conexiones
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 46724,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "sistema_ventas",
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Error conectando a la BD:", err);
-    return;
-  }
-  console.log("✅ Conectado a MySQL");
+// Manejo del cierre del pool
+process.on("SIGINT", async () => {
+  await pool.end();
+  console.log("Conexión a la base de datos cerrada.");
+  process.exit(0);
 });
 
-module.exports = db;
+module.exports = pool;
