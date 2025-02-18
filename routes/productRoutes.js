993b2a2a8304b6ef68782dcd9f2dd8e6038f16ab
Henry-Tercero-MH <tercerohernandezhenrymisael@gmail.com>
2025-02-17 21:33:48 -0600

    se agrego un campo a la tabla productos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -36,15 +36,28 @@ router.post(
   "/",
   verifyToken,
   isAdmin,
-  upload.single("image"),
+  upload.fields([{ name: "image" }, { name: "image_back" }]),
   async (req, res) => {
-    const { name, description, price, stock } = req.body;
-    const image_url = req.file ? `/uploads/${req.file.filename}` : null;
+    const { name, description, price, stock, category_name } = req.body;
+    const image_url = req.files.image
+      ? `/uploads/${req.files.image[0].filename}`
+      : null;
+    const image_url_back = req.files.image_back
+      ? `/uploads/${req.files.image_back[0].filename}`
+      : null;
 
     try {
       await db.query(
-        "INSERT INTO products (name, description, price, stock, image_url) VALUES (?, ?, ?, ?, ?)",
-        [name, description, price, stock, image_url]
+        "INSERT INTO products (name, description, price, stock, image_url, image_url_back, category_name) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        [
+          name,
+          description,
+          price,
+          stock,
+          image_url,
+          image_url_back,
+          category_name,
+        ]
       );
       res.status(201).json({ message: "Producto agregado con éxito" });
     } catch (err) {
